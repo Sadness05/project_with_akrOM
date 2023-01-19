0a8fd73d4d428951a81f53b714e5c30084768bb7
Sadness05 <suxrobjumakulov05@gmail.com>
2023-01-19 20:45:11 +0300

    aside

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,25 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
-     <aside>
-      
+     <aside class="max-w-[257px] relative">
+      <div class="flex gap-2 px-16 py-5 border-b-2">
+        <img src="../src/assets/logo.svg" alt="">
+        <p class="text-2xl font-semibold">Hope UI</p>
+        <div class="absolute -right-3">
+          <img src="../src/assets/strelochka.svg" alt="">
+        </div>
+      </div>
+      <div>
+       <ul>
+        <li>Home</li>
+        <ul>
+          <li>
+            Dashboard
+            <img src="../src/assets/kvadrati.svg" alt="">
+        </li>
+        </ul>
+       </ul>
+      </div>
      </aside>
   </header>
 
